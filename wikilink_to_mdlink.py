f465aa2de3cf8f4ef0459e96ea324cd31ae4c842
James Fitzgerald <james@jawfish.dev>
2023-04-26 15:00:01 -0400

    make wikilink conversion more robust

--- wikilink_to_mdlink.py
+++ wikilink_to_mdlink.py
@@ -6,32 +6,58 @@
 import os
 import re
 
-def process_wikilink(match, file_path):
+def map_directory(directory):
+    file_map = {}
+    for root, dirs, files in os.walk(directory):
+        dirs[:] = [d for d in dirs if not d.startswith('.')]
+        for file in files:
+            if file.lower().endswith('.md'):
+                file_map[file.lower()] = os.path.join(root, file)
+            else:
+                file_map[file.lower()] = os.path.join(root, file)
+    return file_map
+
+def process_wikilink(match, file_path, file_map):
     wikilink = match.group(1)
     if "|" in wikilink:
         filename, title = wikilink.split("|")
     else:
         filename, title = wikilink, wikilink
 
-    filename = filename.replace(" ", "%20")
-    rel_path = os.path.relpath(os.path.join(os.path.dirname(file_path), filename), start=os.path.dirname(file_path))
+    target_filename = filename.lower()
+    target_file_path = None
+    target_extension = ""
+
+    for file_key in file_map:
+        if file_key.startswith(target_filename):
+            target_file_path = file_map[file_key]
+            target_extension = os.path.splitext(file_key)[1]
+            break
+
+    if target_file_path is None:
+        print(f"Warning: File {target_filename} not found for WikiLink '{wikilink}' in file {file_path}.")
+        return f"[{title}]({filename.replace(' ', '%20')}{target_extension})"
+
+    rel_path = os.path.relpath(target_file_path, start=os.path.dirname(file_path)).replace(" ", "%20")
+    print(f"Processing WikiLink '{wikilink}' in file {file_path}: [{title}]({rel_path})")
     return f"[{title}]({rel_path})"
 
-def convert_wikilinks(file_path):
+def convert_wikilinks(file_path, file_map):
     with open(file_path, "r") as file:
         content = file.read()
 
-    content = re.sub(r"\[\[(.*?)\]\]", lambda match: process_wikilink(match, file_path), content)
+    content = re.sub(r"\[\[(.*?)\]\]", lambda match: process_wikilink(match, file_path, file_map), content)
 
     with open(file_path, "w") as file:
         file.write(content)
 
 def process_directory(directory):
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".md"):
-                file_path = os.path.join(root, file)
-                convert_wikilinks(file_path)
+    file_map = map_directory(directory)
+
+    for file_path in file_map.values():
+        if file_path.lower().endswith('.md'):
+            print(f"Processing file: {file_path}")
+            convert_wikilinks(file_path, file_map)
 
 if __name__ == "__main__":
     directory = input("Enter the path to the directory: ")
